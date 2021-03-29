fd3ce46a4fd2b9d62444494ad9ad6786fde33df3
fadeybog2 <fadey2@gmail.com>
2021-03-29 10:35:05 +0300

    G

--- Lab7/Lab7G/main.cpp
+++ Lab7/Lab7G/main.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
+
 using namespace std;
 
-double find_root(double(*f)(double arg), double a, double b, double tol){
-    double m = (a+b)/2;;
-    while(f(m)>1e-6||-f(m)>1e-6){
-        if(f(m)>0) b=m;
-        else if(f(m)<0) a=m;
-        m = (a+b)/2;
-    }
-    return m;
-}
-
-double foo(double x){
-    return -4+55*x;
-}
-int main() {
-    cout<<find_root(foo, -100, 100, 1);
-    return 0;
+int main(){
+    cout<<"YES";
 }
